b57f5ac35a8d082057edcf27e71c77817aa03125
venkatakoteswarao <158768864+venkatakoteswarao@users.noreply.github.com>
2024-12-30 07:18:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import websockets
 import json
 
 # URL of the backend WebSocket server
-BACKEND_URL = "wss://game-hyfq.onrender.com/ws"
+BACKEND_URL = "wss://game1-9l7z.onrender.com/ws" 
 
 # State management
 if "player_name" not in st.session_state:
